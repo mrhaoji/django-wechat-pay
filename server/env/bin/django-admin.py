66dc30e404e358b23074d4e86ab412f5a8689088
HAO CHEN <mrhaoji@gmail.com>
2018-10-23 15:03:23 +0800

    remove env

--- server/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/haoji/Dev/Workspace/DjangoProjects/django-wechat-pay/server/env/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
